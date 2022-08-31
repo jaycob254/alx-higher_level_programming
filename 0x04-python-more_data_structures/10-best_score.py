674c0b66835d24e4fafa0249479af655aea5da7d
Stanley <jaycobstano@gmail.com>
2022-08-31 11:46:57 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary)
+def best_score(my_dict):
+    if my_dict:
+        return (max(my_dict))
