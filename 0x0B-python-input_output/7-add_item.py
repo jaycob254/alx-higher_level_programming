9234f88f0ff7532953af91607ae48895b62fa500
Stanley <jaycobstano@gmail.com>
2022-09-27 12:50:53 -0700

    input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""Defines a JSON file-writing function."""
-import json
+
+"""This module contains ``load_from_json_file()`` function."""
 
 
-def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+def main():
+    """adds all arguments to a Python list, and then save them to a file:"""
+    a_list = []
+
+    for i in range(1, len(sys.argv)):
+        a_list.append(sys.argv[i])
+    try:
+        data = load_from_json_file("add_item.json")
+        if type(data) is list:
+            data.extend(a_list)
+            a_list = data
+    except FileNotFoundError:
+        pass
+    save_to_json_file(a_list, "add_item.json")
+
+
+if __name__ == "__main__":
+    main()
