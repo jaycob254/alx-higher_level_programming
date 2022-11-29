800bbf068061539a999a3af4eb361ee2bcc48cc7
Stanley <jaycobstano@gmail.com>
2022-11-29 01:42:08 -0800

    Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * a script that prints the first argument passed to it:
+ * If no arguments are passed to the script, print “No argument
+ */
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
+}
