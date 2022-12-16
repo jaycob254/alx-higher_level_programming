66c7f14f11c47e27f158cebde7fac9e14f6deb70
Stanley <jaycobstano@gmail.com>
2022-12-15 22:16:38 -0800

    h

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,71 +1,24 @@
 #!/usr/bin/python3
-"""Write a function that finds a peak in a list of unsorted integers."""
-
-
-def peak_rec(arr, l, r):
-    """Recursive way"""
-    if l == r:
-        return arr[l]
-    if r - l == 1:
-        return max(arr[r], arr[l])
-    m = (r + l) // 2
-    if arr[m] < arr[m + 1]:
-        return peak_rec(arr, m + 1, r)
-    return peak_rec(arr, l, m)
-
-
-def find_peak_rec(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
-        return None
-    l = len(list_of_integers)
-    arr = list_of_integers
-    return peak_rec(arr, 0, l - 1)
-
-
-def find_peak_0(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
-        return None
-    l = len(list_of_integers)
-    m = l // 2
-    arr = list_of_integers
-    while True:
-        if m != 0 and m != l - 1 and arr[m - 1] < arr[m] < arr[m + 1]:
-            m = (l + m) // 2
-        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] > arr[m + 1]:
-            m = m // 2
-        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] < arr[m + 1]:
-            m = m // 2
-        else:
-            return arr[m]
+"""This module contains a script that finds a peak in a list of unsorted
+integers"""
 
 
 def find_peak(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
+    """Finds a peak in a list of unsorted integers"""
+    length = len(list_of_integers)
+    if length == 0:
         return None
-    l = len(list_of_integers)
-    m = l // 2
-    arr = list_of_integers
-    while True:
-        if m in (0, l - 1) or arr[m - 1] < arr[m] > arr[m + 1]:
-            return arr[m]
-        if arr[m] < arr[m + 1]:
-            m = (l + m) // 2
-        else:
-            m = m // 2
+    peak = re_peak(list_of_integers, 0, length - 1)
+    return peak
 
-def find_peak_2(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
-        return None
-    p = 0
-    arr = list_of_integers
-    while True:
-        try:
-            if arr[p] > arr[p + 1]:
-                return arr[p]
-            p += 1
-        except IndexError:
-            return arr[p]
+
+def re_peak(int_list, start, end):
+    if start == end:
+        return int_list[start]
+    mid = int((start + end) // 2)
+    if int_list[mid] > int_list[mid + 1] and int_list[mid] > int_list[mid - 1]:
+        return int_list[mid]
+    if int_list[mid + 1] > int_list[mid]:
+        return re_peak(int_list, mid + 1, end)
+    else:
+        return re_peak(int_list, 0, mid - 1)
